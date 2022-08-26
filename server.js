43d80fd5d86a7335ffac5992f9274cad1a98dc78
Martin Grajeda Jr <martingrajedajr@gmail.com>
2022-08-26 11:41:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -48,4 +48,5 @@ app.get("*", (req, res) => {
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  connectDb().then(() => console.log('MongoDb connected'));
 });
